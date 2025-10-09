c1088809270a9efe509bae9eda10925b221bd762
Anand.a.jain <anandajain10@gmail.com>
2025-10-09 15:57:35 +0530

    Update Etree.c

--- Etree.c
+++ Etree.c
@@ -1,265 +1,176 @@
+
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <string.h>
 
 #define MAX 100
-
 typedef struct Node {
-
-char data;
-
-struct Node *left, *right;
-
+    char data;
+    struct Node* left;
+    struct Node* right;
 } Node;
+char stack[MAX];
+int top = -1;
 
-char opStack[MAX];
-
-int opTop = -1;
-
-void pushOp(char ch) {
-
-if (opTop < MAX - 1)
-
-opStack[++opTop] = ch;
-
+void push(char ch) {
+    if (top < MAX - 1)
+        stack[++top] = ch;
 }
 
-char popOp() {
-
-if (opTop >= 0)
-
-return opStack[opTop--];
-
-return '\0';
-
+char pop() {
+    if (top >= 0)
+        return stack[top--];
+    return '\0';
 }
 
-char peekOp() {
-
-if (opTop >= 0)
-
-return opStack[opTop];
-
-return '\0';
-
+char peek() {
+    if (top >= 0)
+        return stack[top];
+    return '\0';
 }
-
-Node* nodeStack[MAX];
-
-int nodeTop = -1;
-
-void pushNode(Node* node) {
-
-if (nodeTop < MAX - 1)
-
-nodeStack[++nodeTop] = node;
-
-}
-
-Node* popNode() {
-
-if (nodeTop >= 0)
-
-return nodeStack[nodeTop--];
-
-return NULL;
-
-}
-
 int isOperand(char ch) {
-
-return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9');
-
+    return ((ch >= 'a' && ch <= 'z') ||
+            (ch >= 'A' && ch <= 'Z') ||
+            (ch >= '0' && ch <= '9'));
 }
 
 int isOperator(char ch) {
-
-return ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '^';
-
+    return (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '^');
 }
 
 int precedence(char op) {
-
-switch (op) {
-
-case '^': return 3;
-
-case '*':
-
-case '/': return 2;
-
-case '+':
-
-case '-': return 1;
-
-default: return 0;
-
-}
-
+    switch (op) {
+        case '^': return 3;
+        case '*':
+        case '/': return 2;
+        case '+':
+        case '-': return 1;
+        default: return 0;
+    }
 }
 
 int isRightAssociative(char op) {
-return op == '^';
+    return (op == '^');
+}
+void infixToPostfix(char infix[], char postfix[]) {
+    int i, k = 0;
+    char ch;
 
+    for (i = 0; i < strlen(infix); i++) {
+        ch = infix[i];
+
+        if (ch == ' ')
+            continue;
+
+        if (isOperand(ch)) {
+            postfix[k++] = ch;
+        }
+        else if (ch == '(') {
+            push(ch);
+        }
+        else if (ch == ')') {
+            while (peek() != '(' && top != -1)
+                postfix[k++] = pop();
+            pop();
+        }
+        else if (isOperator(ch)) {
+            while (isOperator(peek()) &&
+                   ((precedence(ch) < precedence(peek())) ||
+                    (precedence(ch) == precedence(peek()) && !isRightAssociative(ch)))) {
+                postfix[k++] = pop();
+            }
+            push(ch);
+        }
+    }
+
+    while (top != -1) {
+        postfix[k++] = pop();
+    }
+
+    postfix[k] = '\0';
+}
+Node* nodeStack[MAX];
+int nodeTop = -1;
+
+void pushNode(Node* node) {
+    if (nodeTop < MAX - 1)
+        nodeStack[++nodeTop] = node;
 }
 
-Node* createNode(char data) {
-
-Node* newNode = (Node*)malloc(sizeof(Node));
-
-newNode->data = data;
-
-newNode->left = newNode->right = NULL;
-
-return newNode;
-
+Node* popNode() {
+    if (nodeTop >= 0)
+        return nodeStack[nodeTop--];
+    return NULL;
 }
 
-Node* buildExpressionTree(char* infix) {
+Node* newNode(char data) {
+    Node* temp = (Node*)malloc(sizeof(Node));
+    temp->data = data;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+Node* constructTree(char postfix[]) {
+    for (int i = 0; postfix[i] != '\0'; i++) {
+        char ch = postfix[i];
+        Node* node = newNode(ch);
 
-int i;
+        if (isOperand(ch)) {
+            pushNode(node);
+        } else if (isOperator(ch)) {
+            node->right = popNode();
+            node->left = popNode();
+            pushNode(node);
+        }
+    }
 
-char ch;
-
-for (i = 0; i < strlen(infix); i++) {
-
-ch = infix[i];
-
-if (ch == ' ') continue;
-
-if (ch == '(') {
-
-pushOp(ch);
-
+    return popNode();
+}
+void printInorder(Node* root) {
+    if (root != NULL) {
+        if (isOperator(root->data)) printf("(");
+        printInorder(root->left);
+        printf("%c", root->data);
+        printInorder(root->right);
+        if (isOperator(root->data)) printf(")");
+    }
 }
 
-else if (isOperand(ch)) {
-
-Node* operandNode = createNode(ch);
-
-pushNode(operandNode);
-
-}
-
-else if (ch == ')') {
-
-while (peekOp() != '(') {
-
-char op = popOp();
-
-Node* right = popNode();
-
-Node* left = popNode();
-
-Node* opNode = createNode(op);
-
-opNode->left = left;
-
-opNode->right = right;
-
-pushNode(opNode);
-
-}
-
-popOp();
-
-}
-
-else if (isOperator(ch)) {
-
-while (opTop != -1 && peekOp() != '(' &&
-
-(precedence(ch) < precedence(peekOp()) ||
-
-(precedence(ch) == precedence(peekOp()) && !isRightAssociative(ch)))) {
-
-char op = popOp();
-
-Node* right = popNode();
-
-Node* left = popNode();
-
-Node* opNode = createNode(op);
-
-opNode->left = left;
-
-opNode->right = right;
-
-pushNode(opNode);
-
-}
-
-pushOp(ch);
-
-}
-
-}
-
-while (opTop != -1) {
-
-char op = popOp();
-
-Node* right = popNode();
-
-Node* left = popNode();
-Node* opNode = createNode(op);
-
-opNode->left = left;
-
-opNode->right = right;
-
-pushNode(opNode);
-
-}
-
-return popNode();
-
-}
-
-void printPostorder(Node* root) {
-
-if (root == NULL) return;
-
-printPostorder(root->left);
-
-printPostorder(root->right);
-
-printf("%c", root->data);
-
+void printPreorder(Node* root) {
+    if (root != NULL) {
+        printf("%c", root->data);
+        printPreorder(root->left);
+        printPreorder(root->right);
+    }
 }
 
 void freeTree(Node* root) {
-
-if (root == NULL) return;
-
-freeTree(root->left);
-
-freeTree(root->right);
-
-free(root);
-
+    if (root == NULL) return;
+    freeTree(root->left);
+    freeTree(root->right);
+    free(root);
 }
 
 int main() {
+    char infix[MAX], postfix[MAX];
 
-char infix[MAX];
+    printf("Enter Infix expression: ");
+    scanf("%s", infix);
 
-printf("Enter infix expression: ");
+    infixToPostfix(infix, postfix);
+    printf("Postfix expression: %s\n", postfix);
 
-scanf("%s", infix);
+    Node* exprTree = constructTree(postfix);
 
-Node* root = buildExpressionTree(infix);
+    printf("Expression tree (Inorder): ");
+    printInorder(exprTree);
+    printf("\n");
 
-printf("Postfix expression using expression tree: ");
+    printf("Preorder traversal: ");
+    printPreorder(exprTree);
+    printf("\n");
 
-printPostorder(root);
-
-printf("\n");
-
-freeTree(root);
-
-return 0;
-
+    freeTree(exprTree);
+    return 0;
 }
+
+
